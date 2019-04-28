1fbef5e2108a7d1952f2de343d2661a91a0c1e22
Nichole Boothroyd <kitsune.reich@gmail.com>
2019-04-28 16:38:01 -0700

    Scene A

---
+++ Shader HW 1/Assets/moveLight.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveLight : MonoBehaviour
+{
+    public bool moveX;
+    public bool moveY;
+
+    private Vector3 startPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = gameObject.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        if(moveX) {
+            float newX = Mathf.Sin(Time.time) * 3;
+            //print(gameObject.transform.position);
+            gameObject.transform.position = new Vector3(newX, 0f, 0f) + startPos;
+        }
+        else if (moveY) {
+            float newY = Mathf.Sin(Time.time) * 3;
+            //print(gameObject.transform.position);
+            gameObject.transform.position = new Vector3(0f, newY, 0f) + startPos;
+        }
+    }
+}
